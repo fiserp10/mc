f02b39717128beba9807ae8da7f0abb5f7777d9b
Petr Fiser <fiserp10@fel.cvut.cz>
2014-03-17 20:28:35 +0100

    resolve konfliktu

--- src/filemanager/find.c
+++ src/filemanager/find.c
@@ -511,13 +511,12 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
     const char *file_name_label = N_("File name:");
     const char *file_recurs_label = N_("&Find recursively");
     const char *file_pattern_label = N_("&Using shell patterns");
-    const char *file_skip_hidden_label = N_("S&kip hidden");
+    const char *file_skip_hidden_label = N_("&Skip hidden");
     const char *file_only_directories_label = N_("Only &directories");
 #ifdef HAVE_CHARSET
     const char *file_all_charsets_label = N_("&All charsets");
 #endif
     const char *file_case_label = N_("Cas&e sensitive");
-    const char *file_skip_hidden_label = N_("S&kip hidden");
 
     /* file content */
     const char *content_content_label = N_("Content:");
@@ -659,8 +658,9 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
 
     content_use_cbox = check_new (y2++, x2, options.content_use, content_use_label);
     add_widget (find_dlg, content_use_cbox);
-    cbox_position = FIND_Y - 5;
 
+    //cbox_position = FIND_Y - 5;
+    int cbox_position = (y2++) - 5;
     only_directories_cbox = check_new (cbox_position--, 3, options.only_directories, file_only_directories_label);
     add_widget (find_dlg, only_directories_cbox);
 
